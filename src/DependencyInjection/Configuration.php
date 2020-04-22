b5de1d9a0902bd3223668a6046bb011516747183
Khairi Husseini <khusseini@users.noreply.github.com>
2020-04-22 05:13:11 +0200

    Closes #19 (#22)

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -11,6 +11,8 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
  * This is the class that validates and merges configuration from your app/config files.
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/configuration.html}
+ *
+ * @codeCoverageIgnore
  */
 class Configuration implements ConfigurationInterface
 {
